384478236883632a5c8d0db14a0e6d2314d685b7
JorgeRamosS <thorin-70@hotmail.com>
2022-09-05 22:50:20 -0300

    header

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,21 @@
-console.log("Yellow");
+/*=============== SHOW MENU ===============*/
+
+const navMenu = document.getElementById("nav-menu"),
+  navToggle = document.getElementById("nav-toggle"),
+  navClose = document.getElementById("nav-close");
+
+/*===== MENU SHOW =====*/
+/* Validate if constant exists */
+if (navToggle) {
+  navToggle.addEventListener("click", () => {
+    navMenu.classList.add("show-menu");
+  });
+}
+
+/*===== MENU HIDDEN =====*/
+/* Validate if constant exists */
+if (navClose) {
+  navClose.addEventListener("click", () => {
+    navMenu.classList.remove("show-menu");
+  });
+}
